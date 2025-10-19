ef80e0e3c8706595e46f75628a424af4df494784
sKr-1 <72626055+sKr-1@users.noreply.github.com>
2025-10-19 20:56:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+window.onload = function () {
+  let currentIndex = 0;
+  const slides = document.querySelectorAll(".slide");
+
+  function showSlide(index) {
+     slides.forEach((slide) => {
+        slide.style.display = "none";
+     });
+     slides[index].style.display = "block";
+  }
+
+  function nextSlide() {
+     currentIndex = (currentIndex + 1) % slides.length;
+     showSlide(currentIndex);
+  }
+
+  showSlide(currentIndex);
+  setInterval(nextSlide, 3000);
+};
 
